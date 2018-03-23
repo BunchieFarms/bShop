8e5c08e1526e07261b99903009e21b908818b295
Bryce <bohmer@uncc.edu>
2018-03-23 14:36:10 -0400

    Style changes to homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,17 +15,69 @@
 //= require turbolinks
 //= require_tree .
 
-burger =  document.getElementById('hamburger'); 
-menu = document.getElementById('mobileMenu');
 
-function boy() {
-	burger.classList.toggle('change');
 
-	 if (menu.classList.contains('contract')) {
-		menu.classList.remove("contract");
-		menu.classList.add("expand");
-	} else {
-		menu.classList.remove("expand");
-		menu.classList.add("contract");
+window.onload = function () {
+	burger =  document.getElementById('hamburger'); 
+	menu = document.getElementById('mobileMenu');
+	saleBtn = document.getElementById('saleBtn');
+	saleMenu = document.getElementById('sale');
+	housBtn = document.getElementById('housBtn');
+	housMenu = document.getElementById('housing');
+	jobsBtn = document.getElementById('jobsBtn');
+	jobsMenu = document.getElementById('jobs');
+
+	saleBtn.addEventListener("mouseover", mouseOverSale);
+	saleBtn.addEventListener("mouseout", mouseOutSale);
+	saleMenu.addEventListener("mouseout", mouseOutSale);
+	saleMenu.addEventListener("mouseover", mouseOverSale);
+
+	housBtn.addEventListener("mouseover", mouseOverHouse);
+	housBtn.addEventListener("mouseout", mouseOutHouse);
+	housMenu.addEventListener("mouseout", mouseOutHouse);
+	housMenu.addEventListener("mouseover", mouseOverHouse);
+
+	jobsBtn.addEventListener("mouseover", mouseOverJobs);
+	jobsBtn.addEventListener("mouseout", mouseOutJobs);
+	jobsMenu.addEventListener("mouseout", mouseOutJobs);
+	jobsMenu.addEventListener("mouseover", mouseOverJobs);
+
+	function mouseOverSale() {
+	    saleMenu.style.display = "flex";
+	}
+
+	function mouseOutSale() {
+	    saleMenu.style.display = "none";
+	}
+
+	function mouseOverHouse() {
+	    housMenu.style.display = "flex";
+	}
+
+	function mouseOutHouse() {
+	    housMenu.style.display = "none";
+	}
+
+	function mouseOverJobs() {
+	    jobsMenu.style.display = "flex";
+	}
+
+	function mouseOutJobs() {
+	    jobsMenu.style.display = "none";
+	}
+
+	burger.addEventListener("click", btnTransform);
+
+	function btnTransform() {
+		burger.classList.toggle('change');
+
+		 if (menu.classList.contains('contract')) {
+			menu.classList.remove("contract");
+			menu.classList.add("expand");
+		} else {
+			menu.classList.remove("expand");
+			menu.classList.add("contract");
+		}
 	}
 }
+
